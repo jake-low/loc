c5c9f10fc9c32fcf362f614d31c594e9994cdfa4
Jake Low <hello@jakelow.com>
2026-04-02 00:04:34 -0700

    Add GML and GDScript

---
+++ tests/hello.gd
@@ -0,0 +1,8 @@
+A simple GDScript file
+lines=5 code=3 comments=1 blank=1
+---
+# Prints a greeting
+extends Node
+
+func _ready():
+	print("Hello, world!")
---
+++ tests/hello.gml
@@ -0,0 +1,5 @@
+A simple GML script
+lines=2 code=1 comments=1 blank=0
+---
+// Prints a greeting
+show_message("Hello, world!");
